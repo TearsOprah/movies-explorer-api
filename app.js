108063ea4fd73d0e6f1c60ba372b8a15e3b92723
TearsOprah <qmeshokp@mail.ru>
2023-05-30 13:00:48 +0300

    feat: errorHandler и логгирование ошибок и запросов

--- app.js
+++ app.js
@@ -3,27 +3,42 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
 const { errors } = require('celebrate');
-
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+// использование модуля cors
 app.use(cors());
 app.use(helmet());
 
+// подключение к базе данных
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(requestLogger);
 
 app.use(router);
 
+app.use(errorLogger);
 app.use(errors());
+app.use(errorHandler);
 
-app.listen(PORT, (err) => {
-  return err
-    ? console.log(`Ошибка в процессе соединения с портом: ${err}`)
-    : console.log(`Соединение с портом ${PORT} установлено`);
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
